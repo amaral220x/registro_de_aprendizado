c76cea2a9536216bd682d62da6ea35bb87d1ae63
amaral220x <biel.ilha2021@gmail.com>
2020-11-06 10:26:33 -0300

    Criando as páginas de um tópico em específico.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     #Página de tópicos
-    path('topics', views.topics, name='topics')
+    path('topics', views.topics, name='topics'),
+    path('topics/<topic_id>/', views.topic, name='topic')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -9,3 +9,9 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
