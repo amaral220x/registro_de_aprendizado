9ae34782e3af37e248d1a87ddfa7f1eb96d634fa
amaral220x <biel.ilha2021@gmail.com>
2020-11-06 00:15:36 -0300

    Criando as páginas index e topics e base.html

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,4 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     #Página Inicial
     path('', views.index, name='index'),
+
+    #Página de tópicos
+    path('topics', views.topics, name='topics')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
